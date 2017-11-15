fa98ed953f2c6a82aa274667e468ebe7c527d616
Saehan Park <bauser384@gmail.com>
2017-11-15 17:55:50 +0900

    PresaleFirstAdditional finalized

--- contracts/PresaleFirstAdditional.sol
+++ contracts/PresaleFirstAdditional.sol
@@ -4,8 +4,8 @@ import './Pausable.sol';
 import './ERC20Basic.sol';
 import './SafeERC20.sol';
 import './MedToken.sol';
-import './PresaleFirst';
-import './TokenLock';
+import './PresaleFirst.sol';
+import './TokenLock.sol';
 
 contract PresaleFirstAddtional is Pausable {
   using SafeMath for uint256;
@@ -13,9 +13,17 @@ contract PresaleFirstAddtional is Pausable {
   event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
   PresaleFirst public presale;
+  address public fundReceiver;
+  address public tokenOwner;
 
-  function PresaleFirstAddtional(address _presaleAddress) {
+  function PresaleFirstAddtional(
+      address _presaleAddress,
+      address _fundReceiver,
+      address _tokenOwner)
+  {
     presale = PresaleFirst(_presaleAddress);
+    fundReceiver = _fundReceiver;
+    tokenOwner = _tokenOwner;
   }
 
   function validPurchase() internal constant returns (bool) {
@@ -24,14 +32,18 @@ contract PresaleFirstAddtional is Pausable {
     bool nonZeroPurchase = msg.value != 0;
     bool withinCap = (presale.weiRaised() + msg.value) * presale.rate() <= presale.cap();
 
-    return additional && withinPeriod && nonZeroPurchase;
+    return additional && withinPeriod && nonZeroPurchase && !presale.paused();
+  }
+
+  function forwardFunds() internal {
+    fundReceiver.transfer(msg.value);
   }
 
   function () payable {
     buyMore(msg.sender);
   }
 
-  function buyMore(address beneficiary) public payable {
+  function buyMore(address beneficiary) whenNotPaused public payable {
     require(beneficiary != 0x0);
     require(validPurchase());
 
@@ -39,8 +51,10 @@ contract PresaleFirstAddtional is Pausable {
     uint256 tokens = weiAmount.mul(presale.rate());
 
     TokenLock lock = TokenLock(presale.locks(beneficiary));
-    assert(MedToken(presale.token()).transfer(lock, tokens));
+    assert(MedToken(presale.token()).transferFrom(tokenOwner, lock, tokens));
 
     TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+
+    forwardFunds();
   }
 }
